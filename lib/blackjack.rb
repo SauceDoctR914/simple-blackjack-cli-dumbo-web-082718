df4667d8441e559485fdef71b5c1fbb057d34b16
Gavin Soko <sokolofg@gmail.com>
2018-08-12 22:34:30 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -30,8 +30,10 @@ def end_game(cards)
 end
 
 def initial_round(cardtotal)
-  deal_card + deal_card = cardtotal
-  display_card_total
+ card1 = deal_card
+ card2 = deal_card
+ cardtotal = card1 +card2
+ display_card_total(cardtotal)
 end
 
 def hit?
